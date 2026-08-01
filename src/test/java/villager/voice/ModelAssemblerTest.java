91da306c0540e50a70d70e1d5a2054fbde7133a8
Python-Coderss <gurtaj.sran@outlook.com>
2026-08-01 09:58:29 -0600

    Clean up model assembler test imports

--- src/test/java/villager/voice/ModelAssemblerTest.java
+++ src/test/java/villager/voice/ModelAssemblerTest.java
@@ -11,7 +11,6 @@ import java.util.Comparator;
 import java.util.stream.Stream;
 
 import static org.junit.Assert.assertArrayEquals;
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class ModelAssemblerTest {
